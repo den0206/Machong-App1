9fb2d019957dfc9ae794698ff5c48b15f3bf620a
den0206 <mac.pop@mineo.jp>
2019-11-28 13:31:22 +0900

    Initial Commit

---
+++ Machong-App1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Machong-App1
+//
+//  Created by 酒井ゆうき on 2019/11/28.
+//  Copyright © 2019 Yuuki sakai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
